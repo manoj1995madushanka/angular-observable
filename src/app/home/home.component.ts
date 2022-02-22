23b5529422e20ed06c7de1946873931942ce9687
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-02-22 14:56:19 +0530

    define observable and subscribe to it

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {interval, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  firstObservableSubscription: Subscription;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    // interval is build in function that returns count after each 1000ms
+    this.firstObservableSubscription = interval(1000).subscribe(
+      (count) => {
+        console.log(count);
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    // this subscription will close when user navigate to another route
+    this.firstObservableSubscription.unsubscribe();
   }
 
 }
